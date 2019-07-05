ed4d89493552dba935225eb3155bd975de686f4a
Pasindu Senarathne <pasindubahagya@gmail.com>
2019-07-05 11:06:17 +0530

    Swift lesson 1 completed

---
+++ SwiftLearn.playground/Contents.swift
@@ -0,0 +1,46 @@
+import UIKit
+
+var str = "Hello, playground"
+
+print(str)
+
+//for index in 1...5{
+//    print(index);
+//}
+//
+//for _ in 1...5{
+//    print("index");
+//}
+
+//var counter = 4
+//
+//while counter > 0 {
+//
+//    print("index")
+//    counter = counter - 1
+//}
+//
+//repeat{
+//
+//    print("hello")
+//    counter = counter - 1
+//
+//} while counter > 0
+
+//func add(arg num1:Int,arg2 num2:Int) -> Int{
+//    var sum = num1 + num2
+//    return sum
+//
+//}
+//
+//var sum = add(arg:1,arg2:3);
+//print(sum)
+//
+//func add(_ num1:Int,_ num2:Int) -> Int{
+//    var sum = num1 + num2
+//    return sum
+//
+//}
+//
+//var sum2 = add(1,3);
+//print(sum2)
